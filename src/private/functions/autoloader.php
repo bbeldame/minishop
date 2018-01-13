04900148c9eda7ddc5b7f6e517288491e7d389a8
Adrien DELHOM <adrien.delhom@outlook.com>
2018-01-13 22:18:26 +0100

    Correct autloader

--- src/private/functions/autoloader.php
+++ src/private/functions/autoloader.php
@@ -3,7 +3,6 @@
 require_once "category.php";
 require_once "coin.php";
 require_once "configuration.php";
-require_once "database.php";
 require_once "order.php";
 require_once "route.php";
 require_once "user.php";
