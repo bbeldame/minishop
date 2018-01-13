eac8827ff7a849324fb179e8143d3c84e8caef9e
Basile Beldame <basile.beldame@supdeweb-paris.com>
2018-01-13 19:22:51 +0100

    Initial values in sql

--- src/private/functions/configuration.php
+++ src/private/functions/configuration.php
@@ -20,5 +20,6 @@ const ADMIN = 3;
 
 const DEBUG = true;
 const COOKIE_SALT_SECRET = "c172f16bcee5df8ee574e125430c6b0c14989109fac2fe992bfdae8e63c858c6";
+const HASH_SALT = "4318478525a5a4d31d9be92bite80587828";
 
 ?>
