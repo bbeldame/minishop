b9ba3db0e876988cedcbad6f501b26585402cad5
Basile Beldame <basile.beldame@supdeweb-paris.com>
2018-01-14 16:03:18 +0100

    Unit Order query and view done

--- src/private/views/order.php
+++ src/private/views/order.php
@@ -1,34 +1,39 @@
-<div class="page-header">Order #1</div>
+<?php
+    $query = "SELECT cb.quantity, ct.price, ct.name, uot.total_paid
+        FROM coins_bought cb
+        INNER JOIN coins_template ct
+        ON ct.id=cb.coins_template_id
+        INNER JOIN users_orders_template uot
+        ON uot.id=cb.users_orders_template_id
+        WHERE uot.id=".$_GET[2].";";
+    $results = rawQuery($query, true);
+?>
+
+<div class="page-header">Commande #<?= $_GET[2]?> </div>
 
 <div class="table-elements">
     <table cellspacing="0">
         <tr>
             <th>Coin</th>
-            <th>Name</th>
-            <th>Quantity</th>
-            <th>Price</th>
-            <th>More infos</th>
+            <th>Nom</th>
+            <th>Quantité achetée</th>
+            <th>Prix unitaire</th>
+            <th>Prix total</th>
         </tr>
 
-        <tr>
-            <td><img src="https://files.coinmarketcap.com/static/img/coins/32x32/bitcoin.png" alt="" /></td>
-            <td>Bitcoin</td>
-            <td>0.2541</td>
-            <td>$1 254</td>
-
-            <td style="text-align: center"><button>More</button></td>
-        </tr>
-
-        <tr>
-            <td><img src="https://files.coinmarketcap.com/static/img/coins/32x32/ethereum.png" alt="" /></td>
-            <td>Ethereum</td>
-            <td>0.88</td>
-            <td>$800</td>
-            <td style="text-align: center"><button>More</button></td>
-        </tr>
+        <?php
+        foreach ($results as $k => $v) { ?>
+            <tr>
+                <td><img src="https://files.coinmarketcap.com/static/img/coins/32x32/<?= $v["name"] ?>.png" alt="" /></td>
+                <td><?= ucfirst(strtolower ($v['name'])) ?></td>
+                <td><?= $v['quantity'] ?></td>
+                <td><?= $v['price'] ?></td>
+                <td style="text-align:center"><?= $v['quantity'] * $v['price'] ?></td>
+            </tr>
+        <?php } ?> 
     </table>
 </div>
 
 <div class="total-price">
-    <label>Total price : $2 054</label>
+    <label>Prix total de la commande: <?= $results[0]['total_paid'] ?> euros</label>
 </div>
