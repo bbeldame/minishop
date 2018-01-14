626f38e0aa1c1ad95f4dc8e27507184d6bdc6df4
Basile Beldame <basile.beldame@supdeweb-paris.com>
2018-01-14 15:32:01 +0100

    Query for orders done

--- src/private/views/orders.php
+++ src/private/views/orders.php
@@ -1,26 +1,36 @@
-<div class="page-header">Orders</div>
+<?php
+    $query = "SELECT total_paid, paid_date, id
+        FROM users_orders_template
+        WHERE users_template_id=".$_SESSION['id'].";";
+    $results = rawQuery($query, true);
+    if (!$results) { ?>
+        <div align="center" style="margin: 45px auto">
+            Vous n'avez encore rien commandé, hop hop hop il faut s'y mettre !
+        </div>
+    <?php
+        exit;
+    }
+?>
+
+<div class="page-header">Mes commandes</div>
 
 <div class="table-elements">
     <table cellspacing="0">
         <tr>
             <th>#</th>
-            <th>Total price</th>
-            <th>Date</th>
-            <th>More infos</th>
+            <th>Prix payé</th>
+            <th>Date de paiement</th>
+            <th>Détails</th>
         </tr>
 
+        <?php
+            foreach ($results as $k => $v) { ?>
         <tr>
-            <td>1</td>
-            <td>$1 200</td>
-            <td>12/12/2018</td>
-            <td style="text-align: center"><button><a href="/order/1">More</a></button></td>
+            <td><?= $k + 1 ?></td>
+            <td><?= $v['total_paid'] ?> euros</td>
+            <td><?= $v['paid_date'] ?></td>
+            <td style="text-align: center"><button><a href="/order/<?= $v['id'] ?>">Voir</a></button></td>
         </tr>
-
-        <tr>
-            <td>2</td>
-            <td>$3 150</td>
-            <td>18/12/2018</td>
-            <td style="text-align: center"><button><a href="/order/1">More</a></button></td>
-        </tr>
+        <?php } ?>
     </table>
 </div>
